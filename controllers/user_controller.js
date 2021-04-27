783051d100c0af0dcc5e776366efb1736495c393
Ruturaj Chaubey <ruturajchaubey16@gmail.com>
2021-04-27 11:28:38 +0530

    setup user-schema for authentication and rendered sign-in and sign-up pages from the server

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,3 +1,16 @@
 module.exports.profile = function(req, res){
     return res.render('user_profile');
 }
+
+
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up', {
+        title: "Codeial | Sign Up"
+    });
+}
+
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in', {
+        title: "Codeial | Sign In"
+    });
+}
