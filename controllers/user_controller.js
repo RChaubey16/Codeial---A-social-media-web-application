3017a227e482b5b62b50dbdc051da9fa5be3a116
Ruturaj Chaubey <ruturajchaubey16@gmail.com>
2021-05-03 19:00:40 +0530

    user details are now visible in views and also the sign-in and sign-up pages can only be viewed by signed out users

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -12,6 +12,12 @@ module.exports.profile = function(req, res){
 
 // render the sign-up page
 module.exports.signUp = function(req, res){
+
+    if (req.isAuthenticated()){
+        // making sure if only the signed out users are able to view the sign up page and not the signed in ones
+        return res.redirect("/users/profile");
+    }
+
     return res.render('user_sign_up', {
         title: "Codeial | Sign Up"
     });
@@ -20,6 +26,14 @@ module.exports.signUp = function(req, res){
 
 // render the sign-in page
 module.exports.signIn = function(req, res){
+
+    
+    if (req.isAuthenticated()){
+        // making sure if only the signed out users are able to view the sign in page and not the signed in ones
+        return res.redirect("/users/profile");
+    }
+
+
     return res.render('user_sign_in', {
         title: "Codeial | Sign In"
     });
