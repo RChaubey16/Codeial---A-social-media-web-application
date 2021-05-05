8aac5d2acde9922e50d58439568c8e4081181716
Ruturaj Chaubey <ruturajchaubey16@gmail.com>
2021-05-05 16:47:28 +0530

    installed node-sass-middleware and done setting it up

--- index.js
+++ index.js
@@ -10,8 +10,20 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const MongoStore = require("connect-mongo")(session);
+// sass middleware
+const sassMiddleware = require("node-sass-middleware");
 
+// using sass middleware
+app.use(sassMiddleware(
+    {
+        src: "/assets/scss",
+        dest: "/assets/css",
+        debug: true,
+        outputStyle: "extended",
+        prefix: "/css"
+    }
 
+));
 app.use(express.urlencoded());
 
 // Using the cookie-parser lib for cookies
