3adae9ab8d9af08e30a434fc68a44d278fc6b8e7
Ruturaj Chaubey <ruturajchaubey16@gmail.com>
2021-05-07 11:06:52 +0530

    Created a post Schema for posts

---
+++ models/post.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+
+    content:{
+        type: String, 
+        required: true
+    },
+
+    // Establidhing relationship among user and posts
+    user:{
+
+        // takes the ObjectId of the logged in user who's making the post from the db 
+        type: mongoose.Schema.Types.ObjectId,
+        // takes the name of the Schema being followed by the user for other details
+        ref: 'User'
+    }
+},{
+    timestamps: true
+});
+
+const Post = mongoose.model("Post", postSchema);
+module.exports = Post;
