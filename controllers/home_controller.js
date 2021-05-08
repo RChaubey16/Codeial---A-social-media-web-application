44fd931055071714f7206166865e144e4c496b97
Ruturaj Chaubey <ruturajchaubey16@gmail.com>
2021-05-08 13:01:45 +0530

    Displayed the posts, prepopulated the user and displayed the user of the particular post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,13 +1,32 @@
 // module.exports.actionName = function(req, res){}
 
+const Post = require("../models/post");
+
 module.exports.home = function(req, res){
 
     // use of cookies and alteration of cookies in both browser and server. Just an example
     // console.log(req.cookies);
     // res.cookie('something', 'hey!');
     // res.cookie('user_id', 22);
+
+
+    // finding and displaying all the posts
+    // Post.find({}, function(err, posts){
+    //     return res.render('home', {
+    //         title: "Codeial | Home",
+    //         posts: posts
+    //     });
+    // });
+
+    // Populate the user of each post
+    Post.find({}).populate('user').exec(
+        function (err, posts) {
+            return res.render('home', {
+                title: "Codeial | Home",
+                posts: posts
+            });
+        }
+    )
+
     
-    return res.render('home', {
-        title: "Home"
-    });
 }
