958536eaae39c3a0f5d2341ff4597ed1971993d7
Ruturaj Chaubey <ruturajchaubey16@gmail.com>
2021-05-17 14:10:35 +0530

    Created a portal for users to update their profile

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -8,8 +8,17 @@ module.exports.profile = function(req, res){
             profile_user: user
         })
     })
+}
 
-   
+module.exports.update = function (req, res) {
+    
+    if (req.user.id == req.params.id){
+        User.findByIdAndUpdate(req.params.id, {name: req.body.name, email: req.body.email}, function(err, user){
+            return res.redirect('back');
+        });
+    }else{
+        return res.status(401).send("Unauthorized");
+    }
 
 }
 
--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@ const userController = require('../controllers/user_controller');
 const passport = require('passport');
 
 router.get('/profile/:id', passport.checkAuthentication, userController.profile);
+router.post('/update/:id', passport.checkAuthentication, userController.update);
 router.get('/sign-in', userController.signIn);
 router.get('/sign-up', userController.signUp);
 router.post('/create', userController.create);
