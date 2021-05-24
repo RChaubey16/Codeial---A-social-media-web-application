29e12c3b78764426b85c6a802af794fda9782c3c
Ruturaj Chaubey <ruturajchaubey16@gmail.com>
2021-05-24 14:17:16 +0530

    Created and displayed post using AJAX request

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -6,11 +6,21 @@ module.exports.create = async function(req, res){
 
     try {
         
-        await Post.create({
+        let post = await Post.create({
             content: req.body.content,
             user: req.user._id
         });
     
+        // execute of the request is an AJAX request
+        if(req.xhr){
+            return res.status(200).json({
+                data: {
+                    post: post
+                },
+                message: "Post Created"
+            });
+        }
+
         req.flash('success', 'Post Published!')
         return res.redirect('back');
 
