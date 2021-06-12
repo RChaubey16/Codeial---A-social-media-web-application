330738eaa2d003dc40fbc54c38114288ca99dcfb
Ruturaj Chaubey <ruturajchaubey16@gmail.com>
2021-06-12 15:00:51 +0530

    Setup the config file for mailers

---
+++ config/nodemailer.js
@@ -0,0 +1,41 @@
+const nodemailer = require("nodemailer");
+const path = require("path");
+const ejs = require("ejs");
+
+let transporter = nodemailer.createTransport({
+  service: "gmail", // service being used
+  host: "smtp.gmail.com", // the host or protocol to send gmail mails
+  port: 587, // default port number to send TLS secured mails (google - gmail smtp setting and check support.googl.com )
+  secure: false, // currently not secured
+  auth: {
+    // Identity of user who is sender of the mails
+    user: "ruturajchaubey16",
+    pass: "9371618373",
+  },
+});
+
+// to send HTML designed mails
+let renderTemplate = (data, relativePath) => {
+  // data contains user info and relative path is from where the mail is sent (sender's email address)
+  let renderHTML;
+  ejs.render(
+    path.join(__dirname, "../views/mailers", relativePath),
+    data,
+    function (err, template) {
+      if (err) {
+        console.log("Error in rendering the template", err);
+        return;
+      }
+
+      renderHTML = template;
+    }
+  );
+
+  return renderHTML;
+};
+
+// exporting the properties for further use
+modeule.exports = {
+  transporter: transporter,
+  renderTemplate: renderTemplate,
+};
